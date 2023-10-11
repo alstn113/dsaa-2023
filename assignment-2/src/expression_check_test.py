dad5dccb87253ad898885449cfe182b6d6dc121e
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-11 20:21:35 +0900

    refactor: test코드 튜플 순서 수정f

--- assignment-2/src/expression_check_test.py
+++ assignment-2/src/expression_check_test.py
@@ -8,23 +8,26 @@ class ExpressionCheckTest(unittest.TestCase):
 
     def test_expression_validation(self):
         test_list = [
-            ("([[])", False),
-            ("[{(3+2) - [4/(2*1)]} + {2*3}]", True),
-            ("[(3 + 5) * {4 - (7 / 2)} - [8 * (2 + 3) - {9 / (3 + 4)}]]", True),
-            ("{sin[(3 + 5) * (7 - 4)] + cos[2 * π * (3 + 2)]} / {1 + tan[π/4]}", True),
-            ("[{(3 + 5) > (4 - 2)} && {(8 / 2) <= (5 * 2)}] || [{3 * (2 + 1)} == 9]", True),
-            ("arr[[(i + 1) * 3] - {j - 1}] = matrix[2][3 + {k/(2 + 1)}]", True),
-            ("f(g[{h(3 + 5) * 2} - {i(1/2)}], j[k(3 + {l(4 * m(5))})])", True),
-            ("(3 + 5 _ {4 - 2)", False),
-            ("3 + 5) _ {4 - 2}", False),
-            ("[(3 + 5) * {4 - 2)]", False),
-            ("(3 + 5) _ (4 - 2))", False),
-            ("[3 + 5 _ {4 - 2}}]", False),
-            ("{(3 + 5) _ [4 - 2]}", True),
-            ("[3 + 5 _ {4 - 2] - 3}", False)
+            (False, "([[])"),
+            (True, "[{(3+2) - [4/(2*1)]} + {2*3}]"),
+            (True,
+             "[(3 + 5) * {4 - (7 / 2)} - [8 * (2 + 3) - {9 / (3 + 4)}]]"),
+            (True, "{sin[(3 + 5) * (7 - 4)] + cos[2 * π * (3 + 2)]} / {1 + tan[π/4]}"),
+            (True, "[{(3 + 5) > (4 - 2)} && {(8 / 2) <= (5 * 2)}] || [{3 * (2 + 1)} == 9]"),
+            (True,
+             "arr[[(i + 1) * 3] - {j - 1}] = matrix[2][3 + {k/(2 + 1)}]"),
+            (False, "f(g[{h(3 + 5) * 2} - {i(1/2)}], j[k(3 + {l(4 * m(5))}])"),
+            (False, "(3 + 5 _ {4 - 2)"),
+            (False, "3 + 5) _ {4 - 2}"),
+            (False, "[(3 + 5) * {4 - 2)]"),
+            (False, "(3 + 5) _ (4 - 2))"),
+            (False, "[3 + 5 _ {4 - 2}}]"),
+            (True, "{(3 + 5) _ [4 - 2]}"),
+            (False, "[3 + 5 _ {4 - 2] - 3}"),
+
         ]
 
-        for expression, expected_result in test_list:
+        for expected_result, expression in test_list:
             is_valid = self.expression_check_service.checkIsValidExpressionWithHistory(
                 expression)[0]
             self.assertEqual(is_valid, expected_result)
